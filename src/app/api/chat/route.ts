2921fda025de31df537c08809d7aefabcd80c30b
York <57304851+wlyh514@users.noreply.github.com>
2024-05-14 12:42:13 +0800

    feat: Update LLM to gpt-4o

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,23 +15,29 @@ const openai = new OpenAIApi(config);
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { messages } = (await req.json()) as {
-    messages: ChatCompletionRequestMessage[];
-  };
-  if (!messages) {
-    return new Response('', {status: 400});
+
+  try {
+    const { messages } = (await req.json()) as {
+      messages: ChatCompletionRequestMessage[];
+    };
+    if (!messages) {
+      return new Response(null, { status: 400 });
+    }
+
+    // Ask OpenAI for a streaming completion given the prompt
+    const response = await openai.createChatCompletion({
+      model: "gpt-4o",
+      stream: true,
+      temperature: 1,
+      messages: [{ 'role': 'system', 'content': 'You are a helpful AI assistant GPT-4o, you may format your reply in Markdown if necessary. ' }, ...messages],
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    console.error(e);
+    return new Response(null, { status: 500 });
   }
-
-  // Ask OpenAI for a streaming completion given the prompt
-  const response = await openai.createChatCompletion({
-    model: "gpt-4",
-    stream: true,
-    temperature: 1,
-    messages: [{'role': 'system', 'content': 'You are a helpful AI assistant called GPT-4, you may format your reply in Markdown if necessary. '}, ...messages],
-  });
-
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
 }
