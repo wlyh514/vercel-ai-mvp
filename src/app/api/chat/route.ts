3ae710ba5c3bd106a96fe65a5048f1f93d92126e
York <57304851+wlyh514@users.noreply.github.com>
2024-08-26 15:21:24 +0800

    feat: error alert box

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,13 +6,35 @@ import {
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const config = makeConfig();
+
 const openai = new OpenAIApi(config);
 
 // Set the runtime to edge for best performance
 export const runtime = "edge";
+
+function makeConfig(): Configuration {
+  const useAzure = process.env["USE_AZURE"] && ["true", "1"].includes(process.env["USE_AZURE"].toLocaleLowerCase().trim());
+
+  if (useAzure) {
+    return new Configuration({
+      apiKey: process.env["AZURE_OPENAI_API_KEY"],
+      baseOptions: {
+        headers: {
+          "api-key": process.env["AZURE_OPENAI_API_KEY"],
+        }
+      },
+      basePath: `https://${process.env["AZURE_OPENAI_RESOURCE_NAME"]}.openai.azure.com/openai/deployments/${process.env["AZURE_OPENAI_DEPLOYMENT_NAME"]}`,
+      defaultQueryParams: new URLSearchParams({
+        "api-version": process.env["AZURE_OPENAI_API_VERSION"]!, 
+      }),
+    })
+  } else {
+    return new Configuration({
+      apiKey: process.env["OPENAI_API_KEY"],
+    });
+  }
+}
 
 export async function POST(req: Request) {
   const { messages } = (await req.json()) as {
@@ -27,7 +49,7 @@ export async function POST(req: Request) {
     model: "gpt-4",
     stream: true,
     temperature: 1,
-    messages: [{'role': 'system', 'content': 'You are a helpful AI assistant called GPT-4, you may format your reply in Markdown if necessary. '}, ...messages],
+    messages: [{'role': 'system', 'content': 'You are a helpful AI assistant named GPT, you may format your reply in Markdown if necessary. '}, ...messages],
   });
 
   // Convert the response into a friendly text-stream
